255f044c6a0d6a7185ab4015537fd7fffb402110
Martina Mohsen <martinatnmr@gmail.com>
2025-11-15 20:04:41 +0200

    updated the design

--- js/index.js
+++ js/index.js
@@ -11,20 +11,21 @@ $(document).ready(() => {
 
 // !_____________________________________ SideBar__________________________________________________
 
-$(document).ready(function(){
-    $(".open-close-icon").click(function(){
+$(document).ready(function () {
+    $(".open-close-icon").click(function () {
         $(".side-nav-menu").toggleClass("open");
-         $(this).toggleClass("fa-align-justify fa-xmark fa-3x");
+        $(this).toggleClass("fa-align-justify fa-xmark fa-3x");
 
 
-if ($(".side-nav-menu").hasClass("open")) {
-    $(".nav-tab ul li").css({ position: "relative", top: "0px", opacity: 0 }); 
-    $(".nav-tab ul li").each(function (index) {
-        $(this).delay(100 * index).animate({ top: "20px", opacity: 1 }, 300);
-    });} 
-else {
-    $(".nav-tab ul li").animate({ top: "0px", opacity: 0 }, 200);
-}
+        if ($(".side-nav-menu").hasClass("open")) {
+            $(".nav-tab ul li").css({ position: "relative", top: "0px", opacity: 0 });
+            $(".nav-tab ul li").each(function (index) {
+                $(this).delay(100 * index).animate({ top: "20px", opacity: 1 }, 300);
+            });
+        }
+        else {
+            $(".nav-tab ul li").animate({ top: "0px", opacity: 0 }, 200);
+        }
 
     });
 });
@@ -35,7 +36,7 @@ else {
 
 var button = document.querySelector(".logo")
 
-button.addEventListener("click",function(){
+button.addEventListener("click", function () {
     window.location.href = "index.html";
 })
 
@@ -43,22 +44,22 @@ button.addEventListener("click",function(){
 
 // !____________________________display home-page search name api _________________________________________________
 
-var allMeals=[];
-async function getMeals(){
+var allMeals = [];
+async function getMeals() {
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`);
     let data = await response.json();
     console.log(data);
     allMeals = data.meals;
-     displaydata(); 
+    displaydata();
 }
 getMeals();
 
 
-function displaydata(){
-    var cartona ='';
+function displaydata() {
+    var cartona = '';
 
-    for(var i=0; i < allMeals.length; i++ ){
-    cartona += `  <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+    for (var i = 0; i < allMeals.length; i++) {
+        cartona += `  <div class="col-12 col-sm-6 col-md-4 col-lg-3">
   <div  onclick=" getMealDetails('${allMeals[i].idMeal}') "class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
  <img src="${allMeals[i].strMealThumb}" alt="${allMeals[i].strMeal}" class="w-100">
    <div class="meal-layer position-absolute d-flex align-items-center text-black p-2">
@@ -75,7 +76,7 @@ function displaydata(){
 
 
 async function getMealDetails(mealID) {
-   
+
     rowData.innerHTML = ""
     $(".inner-loading-screen").fadeIn(300)
 
@@ -91,7 +92,7 @@ async function getMealDetails(mealID) {
 
 
 function displayMealDetails(meal) {
-    
+
     searchContainer.innerHTML = "";
 
 
@@ -104,7 +105,7 @@ function displayMealDetails(meal) {
     }
 
     let tags = meal.strTags?.split(",")
-   
+
     if (!tags) tags = []
 
     let tagsStr = ''
@@ -147,10 +148,10 @@ function displayMealDetails(meal) {
 
 var searchButton = document.querySelector(".search-button")
 
-searchButton .addEventListener("click",function(){
+searchButton.addEventListener("click", function () {
     showSearchInputs();
-     $(".side-nav-menu").removeClass("open");
-        $(".open-close-icon")
+    $(".side-nav-menu").removeClass("open");
+    $(".open-close-icon")
         .removeClass("fa-xmark")
         .addClass("fa-align-justify");
 
@@ -158,7 +159,7 @@ searchButton .addEventListener("click",function(){
 function showSearchInputs() {
     rowData.innerHTML = ""
     $(".inner-loading-screen").fadeIn(300)
-    
+
     searchContainer.innerHTML = `
     <div class="row py-4 ">
         <div class="col-md-6 ">
@@ -170,11 +171,11 @@ function showSearchInputs() {
     </div>`
 
     rowData.innerHTML = ""
-        $(".inner-loading-screen").fadeOut(300)
+    $(".inner-loading-screen").fadeOut(300)
 }
 
 async function searchByName(term) {
-    
+
     rowData.innerHTML = ""
     $(".inner-loading-screen").fadeIn(300)
 
@@ -187,7 +188,7 @@ async function searchByName(term) {
 }
 
 async function searchByFLetter(term) {
-    
+
     rowData.innerHTML = ""
     $(".inner-loading-screen").fadeIn(300)
 
@@ -225,10 +226,10 @@ function displayMeals(arr) {
 
 var categoriesButton = document.querySelector(".categories-button")
 
-categoriesButton .addEventListener("click",function(){
+categoriesButton.addEventListener("click", function () {
     getCategories();
-     $(".side-nav-menu").removeClass("open");
-        $(".open-close-icon")
+    $(".side-nav-menu").removeClass("open");
+    $(".open-close-icon")
         .removeClass("fa-xmark")
         .addClass("fa-align-justify");
 
@@ -259,7 +260,7 @@ function displayCategories(arr) {
                     <img class="w-100" src="${arr[i].strCategoryThumb}" alt="" srcset="">
                     <div class="meal-layer position-absolute text-center text-black p-2">
                         <h3>${arr[i].strCategory}</h3>
-                        <p>${arr[i].strCategoryDescription.split(" ").slice(0,20).join(" ")}</p>
+                        <p>${arr[i].strCategoryDescription.split(" ").slice(0, 20).join(" ")}</p>
                     </div>
                 </div>
         </div>
@@ -287,10 +288,10 @@ async function getCategoryMeals(category) {
 
 var areabutton = document.querySelector(".area-button")
 
-areabutton.addEventListener("click",function(){
+areabutton.addEventListener("click", function () {
     getArea();
-     $(".side-nav-menu").removeClass("open");
-        $(".open-close-icon")
+    $(".side-nav-menu").removeClass("open");
+    $(".open-close-icon")
         .removeClass("fa-xmark")
         .addClass("fa-align-justify");
 
@@ -346,10 +347,10 @@ async function getAreaMeals(area) {
 
 var ingredientsbutton = document.querySelector(".ingredients-button")
 
-ingredientsbutton.addEventListener("click",function(){
+ingredientsbutton.addEventListener("click", function () {
     getIngredients();
-     $(".side-nav-menu").removeClass("open");
-        $(".open-close-icon")
+    $(".side-nav-menu").removeClass("open");
+    $(".open-close-icon")
         .removeClass("fa-xmark")
         .addClass("fa-align-justify");
 
@@ -380,7 +381,7 @@ function displayIngredients(arr) {
                 <div onclick="getIngredientsMeals('${arr[i].strIngredient}')" class="rounded-2 text-center cursor-pointer">
                         <i class="fa-solid fa-drumstick-bite fa-4x"></i>
                         <h3>${arr[i].strIngredient}</h3>
-                        <p>${arr[i].strDescription.split(" ").slice(0,20).join(" ")}</p>
+                        <p>${arr[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
                 </div>
         </div>
         `
@@ -404,125 +405,137 @@ async function getIngredientsMeals(ingredients) {
 
 // !__________________________contactUs__________________________________________________
 
-var contactbutton = document.querySelector(".contact-button")
+var contactbutton = document.querySelector(".contact-button");
 
-contactbutton.addEventListener("click",function(){
+contactbutton.addEventListener("click", function () {
     displaycontact();
-     $(".side-nav-menu").removeClass("open");
-        $(".open-close-icon")
+    $(".side-nav-menu").removeClass("open");
+    $(".open-close-icon")
         .removeClass("fa-xmark")
         .addClass("fa-align-justify");
+});
 
-})
-
-
-
-
-    function displaycontact() {
-        let cartoona = `
-            <div class="container">
-                <div class="row py-5 g-4" id="rowData">
-                    <div class="contact min-vh-100 d-flex justify-content-center align-items-center">
-                        <div class="container w-75 text-center">
-                            <div class="row g-4">
-                                <div class="col-md-6">
-                                    <input id="nameInput" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Name">
-                                    <div id="nameAlert" class="alert alert-danger w-100 mt-2 d-none">
-                                        Special characters and numbers not allowed
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="emailInput" onkeyup="inputsValidation()" type="email" class="form-control" placeholder="Enter Your Email">
-                                    <div id="emailAlert" class="alert alert-danger w-100 mt-2 d-none">
-                                        Email not valid *exemple@yyy.zzz
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="phoneInput" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Phone">
-                                    <div id="phoneAlert" class="alert alert-danger w-100 mt-2 d-none">
-                                        Enter valid Phone Number
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="ageInput" onkeyup="inputsValidation()" type="number" class="form-control" placeholder="Enter Your Age">
-                                    <div id="ageAlert" class="alert alert-danger w-100 mt-2 d-none">
-                                        Enter valid age
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="passwordInput" onkeyup="inputsValidation()" type="password" class="form-control" placeholder="Enter Your Password">
-                                    <div id="passwordAlert" class="alert alert-danger w-100 mt-2 d-none">
-                                        Enter valid password *Minimum eight characters, at least one letter and one number:*
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="repasswordInput" onkeyup="inputsValidation()" type="password" class="form-control" placeholder="Repassword">
-                                    <div id="repasswordAlert" class="alert alert-danger w-100 mt-2 d-none">
-                                        Enter valid repassword
-                                    </div>
+function displaycontact() {
+    let cartoona = `
+        <div class="container">
+            <div class="row py-5 g-4" id="rowData">
+                <div class="contact min-vh-100 d-flex justify-content-center align-items-center">
+                    <div class="container w-75 text-center">
+                        <div class="row g-4">
+                            <div class="col-md-6">
+                                <input id="nameInput" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Name">
+                                <div id="nameAlert" class="alert alert-danger w-100 mt-2 d-none">
+                                    Special characters and numbers not allowed
                                 </div>
                             </div>
-                            <button id="submitBtn" disabled class="btn btn-outline-danger px-2 mt-3">Submit</button>
+                            <div class="col-md-6">
+                                <input id="emailInput" onkeyup="inputsValidation()" type="email" class="form-control" placeholder="Enter Your Email">
+                                <div id="emailAlert" class="alert alert-danger w-100 mt-2 d-none">
+                                    Email not valid *exemple@yyy.zzz
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <input id="phoneInput" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Phone">
+                                <div id="phoneAlert" class="alert alert-danger w-100 mt-2 d-none">
+                                    Enter valid Phone Number
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <input id="ageInput" onkeyup="inputsValidation()" type="number" class="form-control" placeholder="Enter Your Age">
+                                <div id="ageAlert" class="alert alert-danger w-100 mt-2 d-none">
+                                    Enter valid age
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <input id="passwordInput" onkeyup="inputsValidation()" type="password" class="form-control" placeholder="Enter Your Password">
+                                <div id="passwordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                                    Enter valid password *Minimum eight characters, at least one letter and one number:*
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <input id="repasswordInput" onkeyup="inputsValidation()" type="password" class="form-control" placeholder="Repassword">
+                                <div id="repasswordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                                    Enter valid repassword
+                                </div>
+                            </div>
                         </div>
+                        <button id="submitBtn" disabled class="btn btn-outline-danger px-2 mt-3">Submit</button>
                     </div>
                 </div>
             </div>
-        `;
-        document.getElementById("rowData").innerHTML = cartoona;
+        </div>
+
+        <!-- Simple Toast -->
+       // Simple Toast
+<div id="simpleToast">Your data has been submitted successfully</div>
+
+    `;
+    document.getElementById("rowData").innerHTML = cartoona;
+
+
+    // Submit Button Event
+    document.getElementById("submitBtn").addEventListener("click", function () {
+        let toast = document.getElementById("simpleToast");
+        toast.classList.add("show");
+
+
+        document.getElementById("nameInput").value = "";
+        document.getElementById("emailInput").value = "";
+        document.getElementById("phoneInput").value = "";
+        document.getElementById("ageInput").value = "";
+        document.getElementById("passwordInput").value = "";
+        document.getElementById("repasswordInput").value = "";
+
+        document.getElementById("submitBtn").disabled = true;
+
+        setTimeout(() => {
+            toast.classList.remove("show");
+        }, 3000);
+    });
+}
+function inputsValidation() {
+    const inputs = {
+        name: document.getElementById("nameInput").value,
+        email: document.getElementById("emailInput").value,
+        phone: document.getElementById("phoneInput").value,
+        age: document.getElementById("ageInput").value,
+        password: document.getElementById("passwordInput").value,
+        repassword: document.getElementById("repasswordInput").value
+    };
+
+    const regexPatterns = {
+        name: /^[A-Za-z\s]+$/,
+        email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        phone: /^[0-9]{11}$/,
+        age: /^(1[89]|[2-9][0-9])$/,
+        password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+        repassword: (password) => new RegExp(`^${password}$`)
+    };
+
+    let validInputs = {
+        name: regexPatterns.name.test(inputs.name),
+        email: regexPatterns.email.test(inputs.email),
+        phone: regexPatterns.phone.test(inputs.phone),
+        age: regexPatterns.age.test(inputs.age),
+        password: regexPatterns.password.test(inputs.password),
+        repassword: regexPatterns.repassword(inputs.password).test(inputs.repassword)
+    };
+
+    toggleAlert('nameAlert', validInputs.name);
+    toggleAlert('emailAlert', validInputs.email);
+    toggleAlert('phoneAlert', validInputs.phone);
+    toggleAlert('ageAlert', validInputs.age);
+    toggleAlert('passwordAlert', validInputs.password);
+    toggleAlert('repasswordAlert', validInputs.repassword);
+
+    document.getElementById("submitBtn").disabled = !Object.values(validInputs).every(value => value);
+}
+
+function toggleAlert(alertId, isValid) {
+    const alertElement = document.getElementById(alertId);
+    if (isValid) {
+        alertElement.classList.add('d-none');
+    } else {
+        alertElement.classList.remove('d-none');
     }
-
-    function inputsValidation() {
-        const inputs = {
-            name: document.getElementById("nameInput").value,
-            email: document.getElementById("emailInput").value,
-            phone: document.getElementById("phoneInput").value,
-            age: document.getElementById("ageInput").value,
-            password: document.getElementById("passwordInput").value,
-            repassword: document.getElementById("repasswordInput").value
-        };
-
-     
-        const regexPatterns = {
-            name: /^[A-Za-z\s]+$/,
-            email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            phone: /^[0-9]{11}$/,
-            age: /^(1[89]|[2-9][0-9])$/,
-            password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-            repassword: (password) => new RegExp(`^${password}$`) 
-        };
-
-
-        let validInputs = {
-            name: regexPatterns.name.test(inputs.name),
-            email: regexPatterns.email.test(inputs.email),
-            phone: regexPatterns.phone.test(inputs.phone),
-            age: regexPatterns.age.test(inputs.age),
-            password: regexPatterns.password.test(inputs.password),
-            repassword: regexPatterns.repassword(inputs.password).test(inputs.repassword)
-        };
-
-      
-        toggleAlert('nameAlert', validInputs.name);
-        toggleAlert('emailAlert', validInputs.email);
-        toggleAlert('phoneAlert', validInputs.phone);
-        toggleAlert('ageAlert', validInputs.age);
-        toggleAlert('passwordAlert', validInputs.password);
-        toggleAlert('repasswordAlert', validInputs.repassword);
-
-
-        document.getElementById("submitBtn").disabled = !Object.values(validInputs).every(value => value);
-    }
-
-    function toggleAlert(alertId, isValid) {
-        const alertElement = document.getElementById(alertId);
-        if (isValid) {
-            alertElement.classList.add('d-none');
-        } else {
-            alertElement.classList.remove('d-none');
-        }
-    }
-
- 
-  
-
-
+}
